ba440752e762cf0aa4c6b3b2578543e55f424355
joaobatista235 <juniorbatista0404@gmail.com>
2022-06-25 18:40:58 -0300

    Desenvolvimento da SideBar e Header

---
+++ src/components/Lesson.tsx
@@ -0,0 +1,47 @@
+import { CheckCircle, Lock } from 'phosphor-react'
+import { isPast, format } from 'date-fns'
+
+interface LessonProps {
+    title: string;
+    slug: string;
+    avaliableAt: Date;
+    type: 'live' | 'class';
+}
+
+export function Lesson(props: LessonProps) {
+
+    const isLessonAvailable = isPast(props.avaliableAt);
+    const availableDateFormatted = format(props.avaliableAt, "EEEE' • 'd' de 'MMMM' • 'k'")
+
+    return (
+        <a href="#">
+            <span className="text-gray-300">
+                {props.avaliableAt.toString()}
+            </span>
+            <div className="rouded border border-gray-500 p-4 mt-2">
+                <header className="flex items-center justify-between">
+
+                    {isLessonAvailable ? (
+                        <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+                            <CheckCircle size={20} />
+                            Conteudo Liberado
+                        </span>
+                    ) : (
+                        <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
+                            <Lock size={20} />
+                            Em breve
+                        </span>
+                    )}
+
+
+                    <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 bold font-bold">
+                        {props.type === 'live' ? 'AO VIVO' : 'AULA PRATICA'}
+                    </span>
+                </header>
+                <strong className="text-gray-200 mt-5 block">
+                    {props.title}
+                </strong>
+            </div>
+        </a>
+    )
+}
